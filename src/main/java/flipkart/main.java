44ec22952a538a73a62d7805e7252ec062fac6f7
Siva <Siva@Sivaramakrishna>
2023-07-28 22:45:55 +0530

    First commit

---
+++ src/main/java/flipkart/main.java
@@ -0,0 +1,75 @@
+package flipkart;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.BeforeClass;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.AfterSuite;
+
+public class main{
+	
+	public WebDriver driver;
+	public String flipkarturl="https://www.flipkart.com/";
+	
+	@Test
+	public void flipkartlogin() {
+		
+		WebDriver driver=new ChromeDriver();
+		//System.setProperty("webdriver.chrome.driver", "E:\\sivaa\\Siva_A\\chromedriver_win32.exe");
+		
+		driver.get("https://www.flipkart.com/");
+		driver.manage().window().maximize();	
+		
+	}
+  @Test(dataProvider = "dp")
+  public void f(Integer n, String s) {
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+  }
+
+
+  @DataProvider
+  public Object[][] dp() {
+    return new Object[][] {
+      new Object[] { 1, "a" },
+      new Object[] { 2, "b" },
+    };
+  }
+  @BeforeClass
+  public void beforeClass() {
+  }
+
+  @AfterClass
+  public void afterClass() {
+  }
+
+  @BeforeTest
+  public void beforeTest() {
+  }
+
+  @AfterTest
+  public void afterTest() {
+  }
+
+  @BeforeSuite
+  public void beforeSuite() {
+  }
+
+  @AfterSuite
+  public void afterSuite() {
+  }
+
+}
+
